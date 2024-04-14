925d7270c50eec06572f0baa89a80878360349a4
Pawel Bogdanowicz <pawelb021@gmail.com>
2024-04-14 23:21:01 +0200

    Addes user server support

--- server/server.go
+++ server/server.go
@@ -7,7 +7,9 @@ import (
 
 type Server struct {
 	hosts          map[uuid.UUID]Host
+	users          map[uuid.UUID]User
 	hostSharedLock sync.RWMutex
+	userSharedLock sync.RWMutex
 }
 
 func NewServer() *Server {
@@ -33,6 +35,23 @@ func (s *Server) AddHost(host Host) uuid.UUID {
 	return id
 }
 
+// AddUser Adds the user to the server and returns its uuid
+func (s *Server) AddUser(user User) uuid.UUID {
+	var id uuid.UUID
+	s.userSharedLock.Lock()
+	defer s.userSharedLock.Unlock()
+
+	for {
+		id = uuid.New()
+		if _, ok := s.users[id]; !ok {
+			s.users[id] = user
+			break
+		}
+	}
+
+	return id
+}
+
 // GetHost Returns the host with given uuid. Returns a boolean if the action was a success
 func (s *Server) GetHost(id uuid.UUID) (Host, bool) {
 	s.hostSharedLock.RLock()
@@ -40,4 +59,13 @@ func (s *Server) GetHost(id uuid.UUID) (Host, bool) {
 
 	host, ok := s.hosts[id]
 	return host, ok
+}
+
+// GetUser Returns the user with given uuid. Returns a boolean if the action was a success
+func (s *Server) GetUser(id uuid.UUID) (User, bool) {
+	s.userSharedLock.RLock()
+	defer s.userSharedLock.RUnlock()
+
+	user, ok := s.users[id]
+	return user, ok
 }
