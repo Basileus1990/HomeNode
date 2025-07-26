2eb68a0631222d3e92d32ca5f94e20a383bd1bf4
Pawel Bogdanowicz <pawelb021@gmail.com>
2025-07-26 16:42:21 +0200

    deleted ping testing

--- routing/routing.go
+++ routing/routing.go
@@ -1,13 +1,11 @@
 package routing
 
 import (
-	"github.com/Basileus1990/EasyFileTransfer.git/src/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func SetUpRoutes() *gin.Engine {
 	router := gin.Default()
-	router.GET("/ws", controllers.TestWS)
 
 	api := router.Group("api")
 
--- src/controllers/ping.go
+++ src/controllers/ping.go
@@ -12,10 +12,5 @@ type PingController interface {
 type DefaultPingController struct{}
 
 func (dpc DefaultPingController) Ping(c *gin.Context) {
-	result, err := GlobalConn.Query([]byte("test"))
-	println(string(result))
-	if err != nil {
-		println(err.Error())
-	}
 	c.JSON(http.StatusOK, "Pong :)")
 }
