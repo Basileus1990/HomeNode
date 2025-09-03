ea6baaa870816c7aea4d529dca13c22c9592cd5b
msz <jek_14@wp.pl>
2025-09-03 20:27:05 +0200

    update host and client views

--- frontend/app/client/pages/view-item/view-item.tsx
+++ frontend/app/client/pages/view-item/view-item.tsx
@@ -1,21 +1,21 @@
-import { get } from "http";
 import type { Route } from "./+types/view-item.js";
 
 import RecordsList from "./components/records-list.js";
-import { getItem } from "~/client/service/server-communication.js";
+import { HostWebSocketclient } from "~/client/service/server-com/ws/implemenation.js"
+
 
 export async function clientLoader({ params }: Route.LoaderArgs) {
     if (!params.host_id || !params.item_id) {
         throw new Error("Missing host_id or item_id in route parameters");
     }
-    return getItem(params.host_id, params.item_id);
+    return HostWebSocketclient.getRecordItem(params.host_id, params.item_id);
 }
 
 export default function ViewItem({ loaderData, params }: Route.ComponentProps) {
     return (
         <div>
             <h1>Viewing item #{params.item_id} from host#{params.host_id}</h1>
-            <RecordsList records={loaderData} />
+            <RecordsList records={loaderData} hostId={params.host_id} />
         </div>
     );
 }
--- frontend/app/host/pages/shared-list/components/records-list.tsx
+++ frontend/app/host/pages/shared-list/components/records-list.tsx
@@ -1,12 +1,15 @@
 import { Link, useFetcher } from "react-router";
+import { useContext } from "react";
 
 import { RecordKind, type Items } from "~/common/fs/types";
 import FileRecordListItem from "~/common/components/file-record-listitem.js";
 import DirectoryRecordListItem from "~/common/components/directory-record-listitem.js";
+import { HostIdContext } from "../../host-id-context";
 
 export default function RecordsList({records}: {records: Items.RecordItem[]}) {
     const fetcher = useFetcher();
     const sortedRecords = records.sort((a, b) => a.contentName.localeCompare(b.contentName));
+    const hostId = useContext(HostIdContext);
 
     const deleteItemFetcher = (item: Items.RecordItem) => (
         <fetcher.Form method="post">
@@ -15,19 +18,37 @@ export default function RecordsList({records}: {records: Items.RecordItem[]}) {
         </fetcher.Form>
     )
 
+    const shareLinkButton = (item: Items.RecordItem) => (
+        <button
+            onClick={async () => {
+                const link = `http://localhost:5173/client/${hostId}/${item.recordName}`    // placeholder for dev
+                await navigator.clipboard.writeText(link);
+            }}
+        >
+            Share
+            </button>
+    )
+
     const buildListItem = (record: Items.RecordItem) => {
         if (record.kind === RecordKind.file) {
-            return FileRecordListItem({rec: record as Items.FileRecordItem, children: deleteItemFetcher(record)});
+            return FileRecordListItem({rec: record as Items.FileRecordItem, children: 
+                <>
+                    {deleteItemFetcher(record)}
+                    <br/>
+                    {shareLinkButton(record)}
+                </>
+            });
         } else if (record.kind === RecordKind.directory) {
             return DirectoryRecordListItem({rec: record as Items.DirectoryRecordItem, children:
                 <>
                     {deleteItemFetcher(record)}
                     <Link to={`/host/shared/${(record as Items.DirectoryRecordItem).recordName}`}>View</Link>
+                    <br/>
+                    {shareLinkButton(record)}
                 </>
             });
         }
     };
-
 
     return (
         <div>
